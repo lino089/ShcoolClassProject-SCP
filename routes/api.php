d1a05f4fd8c01c6ab78e911d038ce01a6671c9f4
lino089 <marvellino089@gmail.com>
2026-09-06 00:13:50 +0700

    Menambahkan router/api.php dan mengganti timezone di config/app.php

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/ping', function (){
+    return response()->json([
+        'success' => true,
+        'message' => 'Backend SCP berhasil Merespon',
+        'data' => [
+            'waktu_sekarang' => now()->toDateTimeString()
+
+        ]
+    ]);
+});
