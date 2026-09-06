13b864b3729be8c1598b54880feeefdf4f8f2ee9
lino089 <marvellino089@gmail.com>
2026-09-06 22:22:14 +0700

    Membuat Akun waka

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -25,8 +25,8 @@ return new class extends Migration
             $table->date('graduation_date')->nullable();
             $table->softDeletes('deleted_at');
 
-            // $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name');
+            $table->string('email')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'role' => 'vice_principal',
+            'nis_nip' => '12345678',
+            'password' => Hash::make('password'),
+            'must_change_password' => false,
+            'name' => 'Waka Kurikulum',
         ]);
     }
 }
